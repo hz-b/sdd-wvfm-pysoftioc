c747c21094b3aae433660770902fd9ea0a0eee44
Will Smith <william.smith@helmholtz-berlin.de>
2022-09-29 12:41:08 +0000

    initial commit

--- ioc.py
+++ ioc.py
@@ -1,63 +1,122 @@
 # Import the basic framework components.
+from cothread.catools import caget, connect, camonitor,caput
 from softioc import softioc, builder
 import cothread
+import numpy as np
 
-#import things required for gpio control
+import pickle
+import json
+from os.path import exists
 
-import pigpio
 
-pi1 = pigpio.pi()
-IN_PIN = 17
-OUT_PIN = 18
-PULSE_LEN = 100
+#Read from the save file:
+file_path = 'autosave.json'
+if exists(file_path):
+    with open(file_path) as json_file:
+        autosave = json.load(json_file)
+
+# Or make a new one if we don't have any
+else:
+    autosave = {}
+
 
 # Set the record prefix
-builder.SetDeviceName("SISSY2EX:BIOLOGIC:TRIGGER")
+PREFIX = "SISSY2EX:SDD00:mca1"
+builder.SetDeviceName(PREFIX)
 builder.SetBlocking(True)
 
-trigger_out_counter = builder.longIn('OUTCOUNT', initial_value=0)
-trigger_in_counter = builder.longIn('INCOUNT', initial_value=0)
-done = builder.boolIn('DONE', initial_value=1, ZNAM="Busy", ONAM="Done")
+#Waveform PV for plotting
+energy_waveform = builder.Waveform("ENERGYAXIS",np.linspace(0,1024,1024))
 
-def cbf(g, L, t):
+roi_high_pvs =[]
+roi_low_pvs =[]
 
-    """
-    A function which will be called when the input pin is triggered
-    """
-    trigger_in_counter.set(trigger_in_counter.get()+1)
-    done.set(1)
-
-
-def send_trigger(v):
-
-    """
-    A function which will be called when the output is triggered
-    """
-
-    pi1.gpio_trigger(OUT_PIN, PULSE_LEN, 1)
-    trigger_out_counter.set(trigger_out_counter.get()+1) 
-    done.set(0)
+for i in range(32):
+    roi_high_pvs.append(builder.aOut("R"+str(i)+"HIENERGY",EGU= "eV",DRVL=-237, DRVH=9918,initial_value= 0, on_update_name=lambda v,name: update_roi(v,name)))
+    roi_low_pvs.append(builder.aOut("R"+str(i)+"LOENERGY",EGU="eV",DRVL=-237, DRVH=9918,initial_value= 0, on_update_name=lambda v,name: update_roi(v,name)))
     
-# Create some records
-out_pin_rb = builder.boolIn('OUTRB', ZNAM="Off", ONAM="On")
-out_pin_sp = builder.boolOut('OUTSP', ZNAM="Off", ONAM="On",initial_value=0,always_update=True, on_update=lambda v: pi1.write(OUT_PIN,v))
-trigger = builder.boolOut('SEND', initial_value =0,on_update=lambda v: send_trigger(v),always_update=True)
+values = [0,0,0,"",0]
+
+def update_autosave(pv_name, value):
+
+    autosave[pv_name] = value
+    with open(file_path, 'w') as fp:
+        json.dump( autosave,fp)
+
+def update_roi(energy,py_pv_name):
+
+    #Determine the PV name we need to write to
+    pv_name = py_pv_name.split(PREFIX+":")[1]
+    pv_name = pv_name[:-6]
+    pv_name = PREFIX + "." + pv_name
+    
+    #Now determine the appropriate offset 
+    offset = values[0]
+    slope = values[1]
+    quadratic = values[2]
+
+    #ax2 + bx + c = 0 
+    a=quadratic
+    b=slope
+    c=offset-energy
+
+    channel_p = (-b + np.sqrt(b*b - 4*a*c))/ (2*a)
+    channel_n = (-b - np.sqrt(b*b - 4*a*c))/ (2*a)
+
+    if channel_p > 0 :
+        channel = channel_p
+    else:
+        channel = channel_n
+    
+    
+    channel = int(np.floor(channel))
+    if channel <0:
+        channel = 0
+    
+    caput(pv_name, channel)
+
+    ## Save to file
+    update_autosave(py_pv_name,energy)
+    
+
+#set up the camonitors
+"""
+    offset = Cpt(EpicsSignalRO, '.CALO',kind='config')
+    slope = Cpt(EpicsSignalRO, '.CALS',kind='config')
+    quadratic = Cpt(EpicsSignalRO, '.CALQ',kind='config')
+    egu = Cpt(EpicsSignalRO, '.EGU',kind='config')
+    two_theta = Cpt(EpicsSignalRO, '.TTH',kind='config')
+"""
+monitor_pvs = [PREFIX + ".CALO",PREFIX + ".CALS",PREFIX + ".CALQ",PREFIX + ".EGU",PREFIX + ".TTH"]
+
+
+def calc_waveform(value, index):
+
+    values[index] = value
+    offset = values[0]
+    slope = values[1]
+    quadratic = values[2]
+    egu = values[3]
+    two_theta = values[4]
+
+    channels = np.linspace(0,1024,1024)
+    energy = quadratic*channels**2 + slope*channels+offset
+    energy_waveform.set(energy)
+
+
+camonitor(monitor_pvs,calc_waveform)
 
 # Boilerplate get the IOC started
 builder.LoadDatabase()
 softioc.iocInit()
 
 # Start processes required to be run after iocInit
-def update():
-    while True:
-        out_pin_rb.set(pi1.read(OUT_PIN))
-        cothread.Sleep(0.1)
 
+# Restore autosave positions
+for pv_name in autosave:
 
-cothread.Spawn(update)
+    caput(pv_name, autosave[pv_name])
 
-#attach calback to the input pin on the rising edge
-cb = pi1.callback(IN_PIN, pigpio.RISING_EDGE, cbf)
 
 # Finally leave the IOC running with an interactive shell.
 softioc.interactive_ioc(globals())
--- start_ioc.sh
+++ start_ioc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PIGPIO_ADDR=172.17.9.115 python3 ioc.py
+python3 ioc.py
 
